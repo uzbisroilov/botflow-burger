03327b6e3baefd744cbf7b39a06c640b75fc3828
itsladykerrtoo <itsladykertoo@email.com>
2026-06-01 23:20:16 +0500

    Fix menu fallback

--- services/menuService.js
+++ services/menuService.js
@@ -13,7 +13,20 @@ async function ensureMenuFile() {
 
 async function getMenus() {
   await ensureMenuFile();
-  return await fs.readJson(MENU_FILE);
+
+  try {
+    const menus = await fs.readJson(MENU_FILE);
+
+    if (!menus || Object.keys(menus).length === 0) {
+      await fs.writeJson(MENU_FILE, restaurants, { spaces: 2 });
+      return restaurants;
+    }
+
+    return menus;
+  } catch (error) {
+    await fs.writeJson(MENU_FILE, restaurants, { spaces: 2 });
+    return restaurants;
+  }
 }
 
 async function saveMenus(menus) {
@@ -23,24 +36,33 @@ async function saveMenus(menus) {
 async function updateMenuItem(restaurantId, itemKey, name, price) {
   const menus = await getMenus();
 
+  if (!menus[restaurantId]) return null;
+
   menus[restaurantId].menu[itemKey] = {
     name,
     price: Number(price),
   };
 
   await saveMenus(menus);
+
+  return menus[restaurantId];
 }
 
 async function deleteMenuItem(restaurantId, itemKey) {
   const menus = await getMenus();
 
+  if (!menus[restaurantId]) return null;
+
   delete menus[restaurantId].menu[itemKey];
 
   await saveMenus(menus);
+
+  return menus[restaurantId];
 }
 
 module.exports = {
   getMenus,
+  saveMenus,
   updateMenuItem,
   deleteMenuItem,
 };
